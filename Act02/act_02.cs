806e9039d05fe6c693f666c681b5a831282fe389
LauraFontana01 <fontanameiolaura@hotmail.com>
2025-03-12 17:59:18 -0300

    c# - act 02

---
+++ Act02/act_02.cs
@@ -0,0 +1,39 @@
+﻿// BRUNO C. RODGERS, 12/03/2025
+// Receive 3 numbers and determine biggest number
+
+using System;
+
+class Act02
+{
+    static void Main()
+    {
+        // Receive 3 numbers
+        Console.Write("Enter 1º number: ");
+        double num_1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter 2º number: ");
+        double num_2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter 3º numver: ");
+        double num_3 = Convert.ToDouble(Console.ReadLine());
+
+        // Determine biggest number
+        double biggest;
+
+        if (num_1 >= num_2 && num_1 >= num_3)
+        {
+            biggest = num_1;
+        }
+        else if (num_2 >= num_1 && num_2 >= num_3)
+        {
+            biggest = num_2;
+        }
+        else
+        {
+            biggest = num_3;
+        }
+
+        // Show biggest number
+        Console.WriteLine($"The biggest number is: {biggest}");
+    }
+}
